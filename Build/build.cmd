892841d62ad53d624213f60a8909ffa3a5777ae3
Victor Procure (Contractor) <victor.procure@sjrb.ca>
2018-08-01 08:26:34 -0600

    Fixed build scripts

--- Build/build.cmd
+++ Build/build.cmd
@@ -1,14 +1,25 @@
 @ECHO OFF
 
+if "%1" == "" goto no_config
+if "%1" NEQ "" goto set_config
+
+:set_config
+SET Configuration=%1
+GOTO build
+
+:no_config
+SET Configuration=Release
+GOTO build
+
 :build
 echo ------------------
 echo  Building Package 
 echo ------------------
-dotnet build ..\Source\Prism.CastleWindsor\
+dotnet build .\Source\Prism.CastleWindsor\ -c %Configuration% 
 GOTO test
 
 :test
 echo ---------------
 echo  Running Tests 
 echo ---------------
-dotnet test ..\Source\Prism.CastleWindsor.Wpf.Tests\
+dotnet test .\Source\Prism.CastleWindsor.Wpf.Tests\ || exit /b 1
